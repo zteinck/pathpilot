441e703d1017633d94ae780ef1f9bda0a551c736
Zachary Einck <zacharyeinck@gmail.com>
2024-06-04 22:28:18 -0500

    revised directory structure and added pyproject.toml

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .pathpilot import *
-
-__version__ = '0.1.0'
-__author__ = 'Zachary T. Einck'
---
+++ pathpilot/__init__.py
@@ -0,0 +1,4 @@
+from .pathpilot import *
+
+__version__ = '0.1.0'
+__author__ = 'Zachary Einck <zacharyeinck@gmail.com>'
